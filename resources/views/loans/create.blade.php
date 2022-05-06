aea4df2dda45cd1fc7693baada24d8d7d2f20360
JosephAdewole <josephadewole1@gmail.com>
2022-05-07 00:58:56 +0100

    collected request variables from user and consumed SOAP api

--- resources/views/loans/create.blade.php
+++ resources/views/loans/create.blade.php
@@ -2,14 +2,30 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <form action="{{url('loans')}}" method="POST">
-                @csrf
-                <input type="text" class="form-input" name= 'loan_amount'>
-                <input type="text" class="form-input" name= 'interest_rate'>
-                <input type="text" class="form-input" name= 'period'>
-                <input type="submit" value="Get Loan Details">
-            </form>
-        </div>
+        
+        @if ((session('amount_to_be_paid_back')))
+            <div class="row">
+                {{session('amount_to_be_paid_back')}}
+            </div> 
+            <div class="row">
+                <form action="{{route('loans.store')}}" method="post">
+                    @csrf
+                    <input type="hidden" class="form-input" name= 'loan_amount' value="{{session('loan_amount')}}">
+                    <input type="hidden" class="form-input" name= 'interest_rate' value="{{session('interest_rate')}}">
+                    <input type="hidden" class="form-input" name= 'period' value="{{session('period')}}">
+                    <input type="submit" value="Get Loan">
+                </form>
+            </div>
+        @else
+            <div class="row">
+                <form action="{{url('calculate_loan')}}" method="POST">
+                    @csrf
+                    <input type="text" class="form-input" name= 'loan_amount'>
+                    <input type="text" class="form-input" name= 'interest_rate'>
+                    <input type="text" class="form-input" name= 'period'>
+                    <input type="submit" value="Get Loan Details">
+                </form>
+            </div>
+        @endif
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\WorkController@index');
 
 Route::resource('loans', 'App\Http\Controllers\LoanapplicationController');
+Route::post('calculate_loan', [App\Http\Controllers\LoanapplicationController::class, 'calculate_loan']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Auth::routes();
